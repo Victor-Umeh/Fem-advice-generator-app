300b5e63891c6ed25e8dd6244e2a3d1b01ac8eb9
vk196 <victorumeh196@gmail.com>
2023-06-09 05:58:38 -0700

    added data storage and retrieval funtionality

--- advice-generator-app-main/dist/app.js
+++ advice-generator-app-main/dist/app.js
@@ -3,6 +3,16 @@ const btnEl = document.querySelector("button");
 const quoteEl = document.getElementById("quotes");
 const quoteNum = document.getElementById("quote-num");
 
+function displayData(data) {
+  //Destructure response object
+  const {
+    slip: { id, advice },
+  } = data;
+
+  //Display data
+  quoteEl.textContent = `"${advice}"`;
+  quoteNum.textContent = `#${id}`;
+}
 function getAdvice() {
   fetch("https://api.adviceslip.com/advice", {
     cache: "no-cache",
@@ -12,18 +22,20 @@ function getAdvice() {
       return res.json();
     })
     .then((data) => {
-      //Destructure response object
-      const {
-        slip: { id, advice },
-      } = data;
-
-      //Display data
-      quoteEl.textContent = `"${advice}"`;
-      quoteNum.textContent = `#${id}`;
+      //Store data in locale storage
+      const str = localStorage.setItem("data", JSON.stringify(data));
+      displayData(data);
     })
     .catch((err) => {
       quoteEl.textContent = err.message;
     });
 }
 
+//Get data on button click
 btnEl.addEventListener("click", getAdvice);
+
+//On page reload get last fetched advice and display
+window.addEventListener("load", () => {
+  const advice = JSON.parse(localStorage.getItem("data"));
+  displayData(advice);
+});
